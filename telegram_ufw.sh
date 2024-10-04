1cce2797cf13437245d291d0127e1ccd7070c3a4
ms101 <ms101@users.noreply.github.com>
2024-10-04 02:53:07 -0600

    tmsg func

--- telegram_ufw.sh
+++ telegram_ufw.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 BOTID="" # bot token from botfather
 CHATID="" # find with https://api.telegram.org/bot<YourBOTToken>/getUpdates
 URL="https://api.telegram.org/bot$BOTID/sendMessage?chat_id=$CHATID"
@@ -6,17 +7,21 @@ DATE=$(date '+%Y-%m-%d %H:%M:%S')
 HOST=$(hostname)
 TITLE="$DATE $HOST"
 
+tmsg () {
+        curl -s -X POST -d "text=$TITLE %0A$1" "$URL"
+}
+
+command -v ufw || tmsg "ufw not found"
+
 UFWS=$(ufw status | grep "Status:" | cut -d" " -f2)
 UFWP=$(ufw status | grep ALLOW | grep -v 22)
 
 if [ "$UFWS" = "inactive" ]
 then
-        curl -X POST \
-                -d "text=$TITLE %0Aufw inactive!" \
-                "$URL" #&>/dev/null
+        echo "$DATE ufw inactive"
+        tmsg "ufw inactive"
 elif [ -n "$UFWP" ]
 then
-        curl -X POST \
-                -d "text=$TITLE %0A$UFWP" \
-                "$URL" #&>/dev/null
+        echo "$DATE open ports found"
+        tmsg "$UFWP"
 fi
